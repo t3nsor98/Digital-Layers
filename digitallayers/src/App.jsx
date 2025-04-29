b9a8a2a5a696f260d0e09accef437e968177af8c
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-04-29 21:29:40 +0530

    feat: add Pricing component to display pricing plans

--- digitallayers/src/App.jsx
+++ digitallayers/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-
+import Pricing from "./components/Pricing";
 function App() {
   return (
     <>
       <Hero />
       <Services />
+      <Pricing />
     </>
   );
 }
