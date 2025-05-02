e9a3020afc2c1f46d13658482319147669638900
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-05-02 21:54:49 +0530

    feat: integrate Github and Pricing components into App

--- digitallayers/src/App.jsx
+++ digitallayers/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
+import Github from "./components/Github";
 
+import Pricing from "./components/Pricing";
 function App() {
   return (
     <>
       <Hero />
       <Services />
+      <Github />
+      <Pricing />
     </>
   );
 }
